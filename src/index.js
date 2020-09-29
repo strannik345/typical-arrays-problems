a54d9215beaea50edcfe01074dc0a09c2944a17a
Володько Василий Васильевич <C:\Users\volodko\AppData\Roaming\The Bat!>
2020-09-29 15:17:27 +0300

    feat: done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  let min = array[0];
+  for(let i = 0;i < array.length; i++)
+  {
+    if(array[i]<min) min = array[i];
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  let max = array[0];
+  for(let i = 0;i < array.length; i++)
+  {
+    if(array[i]>max) max = array[i];
+  }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == undefined) return 0;
+  if (array.length == 0) return 0;
+  let avg = 0;
+  for(let i = 0;i < array.length; i++)
+  {
+    avg += array[i];
+  }
+  return avg/array.length;
 }
